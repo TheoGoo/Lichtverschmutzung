75de19f7478bd5e457f4885249dc4f80c95f4832
Theo Gosch <th.gosch@gmx.de>
2022-07-23 02:10:57 +0200

    Comments and temporary resize fuction

--- src/de/theo/main/MainGUI.java
+++ src/de/theo/main/MainGUI.java
@@ -9,29 +9,33 @@ import java.awt.event.MouseMotionAdapter;
 
 public class MainGUI {
 
+    //create JFrame in class to make it accessible to outside the main method
     static JFrame frame = new JFrame("GText");
+    //stores initial position of the mouse cursor (can be used for moving windows and maybe more in the future)
     int pX, pY;
 
     public MainGUI() {
+
+        /*
+        Move the entire window around without using the default "decoration"
+         */
         TitleBar.addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent me) {
                 // Get x,y and store them
                 pX = me.getX();
                 pY = me.getY();
             }
-            public void mouseDragged(MouseEvent me) {
-
-                frame.setLocation(frame.getLocation().x + me.getX() - pX,
-                        frame.getLocation().y + me.getY() - pY);
-            }
         });
         TitleBar.addMouseMotionListener(new MouseMotionAdapter() {
             public void mouseDragged(MouseEvent me) {
-
+                //calculate new window position
                 frame.setLocation(frame.getLocation().x + me.getX() - pX,
                         frame.getLocation().y + me.getY() - pY);
             }
         });
+        /*
+        Basic functionality like minimize and close buttons
+         */
         CloseButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -46,6 +50,17 @@ public class MainGUI {
                 frame.setState(Frame.ICONIFIED);
             }
         });
+        /*
+        Temporary solution for resizing the main window
+         */
+        MainWindow.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                super.mouseDragged(e);
+                //get curser position and set as size
+                frame.setSize(e.getX(),e.getY());
+            }
+        });
     }
 
     public static void main(String[] args) {
@@ -57,6 +72,9 @@ public class MainGUI {
         frame.setVisible(true);
     }
 
+    /*
+    Setting the icon for the task bar
+     */
     static void setIcon(){
         java.net.URL url = ClassLoader.getSystemResource("de/theo/icons/gtext_icon.jpg");
         Toolkit kit = Toolkit.getDefaultToolkit();
@@ -70,4 +88,5 @@ public class MainGUI {
     private JLabel AppName;
     private JLabel CloseButton;
     private JLabel MinimizeButton;
+    private JPanel WindowContent;
 }
