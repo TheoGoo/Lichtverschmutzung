c2dde91415e1a79d17dacca08b20e5afd072d9b7
Theo Gosch <th.gosch@gmx.de>
2022-09-25 15:00:36 +0200

    NameChange

--- src/de/theo/main/MainGUI.java
+++ src/de/theo/main/MainGUI.java
@@ -1,14 +1,18 @@
 package de.theo.main;
 
 import com.formdev.flatlaf.intellijthemes.FlatXcodeDarkIJTheme;
+import com.formdev.flatlaf.json.Json;
 import de.theo.configs.ConfigCreate;
 import de.theo.configs.ReadConfig;
 import de.theo.configs.UpdateConfig;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.Console;
 
 public class MainGUI {
 
@@ -21,18 +25,20 @@ public class MainGUI {
                 new UpdateConfig().createEntry("username",Username.getText());
             }
         });
-        ReadTest.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-                ReadTest.setText(new ReadConfig().readConfig("username"));
-            }
-        });
         RemoveUsername.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
                 new UpdateConfig().deleteEntry("username");
+            }
+        });
+        SendMessage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String bababoi_text = ChatField.getText();
+                String jsonRequest =
+                        "{" + "username: " + new ReadConfig().readConfig("username") + "}";
+                System.out.println("username");
             }
         });
     }
@@ -74,4 +80,7 @@ public class MainGUI {
     private JTextField Username;
     private JButton ReadTest;
     private JButton RemoveUsername;
+    private JTextField ChatField;
+    private JButton SendMessage;
+    private JTextArea textArea1;
 }
