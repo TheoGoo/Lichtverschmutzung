78d4d3be649d8360c7e484e3e01d3638c79b3d47
Theo Gosch <th.gosch@gmx.de>
2022-09-25 15:55:34 +0200

    Settings

--- src/de/theo/main/KameraEinstellungen.java
+++ src/de/theo/main/KameraEinstellungen.java
@@ -1,32 +1,43 @@
 package de.theo.main;
 
+import de.theo.configs.ReadConfig;
 import de.theo.configs.UpdateConfig;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.math.BigDecimal;
 
 public class KameraEinstellungen {
     JPanel Einstellungen;
-    private JButton okBrennweite;
-    private JButton okPixel;
     private JTextField brennweiteEingabe;
     private JTextField pixelEingabe;
+    private JButton okButton;
 
     public KameraEinstellungen() {
-        okBrennweite.addActionListener(new ActionListener() {
+
+        okButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 UpdateConfig config = new UpdateConfig();
                 config.createEntry("Brennweite",brennweiteEingabe.getText());
-            }
-        });
-        okPixel.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                UpdateConfig config = new UpdateConfig();
                 config.createEntry("Pixel",pixelEingabe.getText());
+                config.createEntry("Abbildungsmassstab",Abbildungsmassstab());
             }
         });
     }
+
+    private String Abbildungsmassstab(){
+        ReadConfig readConfig = new ReadConfig();
+        BigDecimal bigDecimal = new BigDecimal("206").multiply(new BigDecimal(readConfig.readConfig("Pixel"))).divide(new BigDecimal(readConfig.readConfig("Brennweite")));
+        String abbildungsmassstab = bigDecimal.toString();
+        /*
+        ---------------------------------------------------------
+        |https://www.sterngucker.de/astrofotografie/sampling/   |
+        ---------------------------------------------------------
+         */
+
+        return abbildungsmassstab;
+    }
+
 }
