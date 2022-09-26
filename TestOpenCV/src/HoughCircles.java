e8bebaf19fb5e4097d053967284ddb1ed850fac5
Theo Gosch <th.gosch@gmx.de>
2022-09-26 13:38:21 +0200

    Test

---
+++ TestOpenCV/src/HoughCircles.java
@@ -0,0 +1,9 @@
+import org.opencv.core.Core;
+
+public class HoughCircles {
+    public static void main(String[] args) {
+        // Load the native library.
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        new HoughCirclesRun().run(args);
+    }
+}
